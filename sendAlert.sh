c294e264288db21e6dcd0d31a9e8f4750bb0c461
andrii.k <andrii.k@opsworks.co>
2020-09-09 20:51:05 +0300

    Adding Alert script

---
+++ sendAlert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+name=$RANDOM
+url='http://127.0.0.1:9093/api/v1/alerts'
+echo "firing up alert $name" 
+# change url o
+curl -XPOST $url -d "[{ 
+        \"status\": \"firing\",
+        \"labels\": {
+                \"alertname\": \"$name\",
+                \"service\": \"my-service\",
+                \"severity\":\"warning\",
+                \"instance\": \"$name.example.net\"
+        },
+        \"annotations\": {
+                \"summary\": \"High latency is high!\"
+        },
+        \"generatorURL\": \"http://prometheus.int.example.net/<generating_expression>\"
+}]"
+echo ""
+echo "press enter to resolve alert"
+read
+echo "sending resolve"
+curl -XPOST $url -d "[{ 
+        \"status\": \"resolved\",
+        \"labels\": {
+                \"alertname\": \"$name\",
+                \"service\": \"my-service\",
+                \"severity\":\"warning\",
+                \"instance\": \"$name.example.net\"
+        },
+        \"annotations\": {
+                \"summary\": \"High latency is high!\"
+        },
+        \"generatorURL\": \"http://prometheus.int.example.net/<generating_expression>\"
+}]"
+echo ""
